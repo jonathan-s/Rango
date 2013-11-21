f88c0e552c47ad002affbf1e17e41489ee6dcbaa
Jonathan Sundqvist <sundqvist.jonathan@gmail.com>
2013-11-21 15:50:08 +0100

    Chapter 7 implemented

--- tango/rango/views.py
+++ tango/rango/views.py
@@ -3,13 +3,62 @@ from django.template import RequestContext
 from django.shortcuts import render_to_response
 
 from rango.models import Category, Page
+from rango.forms import CategoryForm, PageForm
+
+def decode_url(url):
+    name = url.replace('_', ' ')
+    return name
+
+def add_page(request, category_name_url):
+    context = RequestContext(request)
+
+    category_name = decode_url(category_name_url)
+
+    if request.method == 'POST':
+        form = PageForm(request.POST)
+
+        if form.is_valid():
+
+            page = form.save(commit=False)
+
+            cat = Category.objects.get(name=category_name)
+            page.category = cat
+
+            page.views = 0
+
+            page.save()
+            return category(request, category_name_url)
+        else:
+            print form.errors
+    else:
+        form = PageForm()
+
+    return render_to_response('rango/add_page.html', {'category_name_url': category_name_url, 'category_name': category_name, 'form': form }, context)
+
+
+
+def add_category(request):
+    context = RequestContext(request)
+
+    if request.method == 'POST':
+        form = CategoryForm(request.POST)
+
+        if form.is_valid():
+            form.save(commit=True)
+            return index(request)
+        else:
+            print form.errors
+    else:
+        # if request was a GET, display form to enter details. 
+        form = CategoryForm()
+    return render_to_response('rango/add_category.html', {'form': form }, context)
 
 def category(request, category_name_url):
     context = RequestContext(request)
 
-    category_name = category_name_url.replace('_', ' ')
+    category_name = decode_url(category_name_url)
 
-    context_dict = {'category_name': category_name}
+    context_dict = {'category_name': category_name, 'category_name_url': category_name_url}
 
     try:
         category = Category.objects.get(name=category_name)
